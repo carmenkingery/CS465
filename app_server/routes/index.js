4f4f6b02d8e292c4185c5fbe229e97b03b3ee4f6
Carmen Kingery <carmenkingery@gmail.com>
2023-11-08 02:16:47 -0600

    Covert travel template from static data to dynamic JSON

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const controller = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', controller.index);
+router.get('/', ctrlMain.index);
 
 module.exports = router;
